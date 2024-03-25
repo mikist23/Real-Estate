a3fca73fb6e4adb442673b23bf5ee11dbaa353e4
@Mikist <mikeochango3@gmail.com>
2024-03-25 18:35:35 +0300

    created creating user api

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-//import { userRoute } from './routes/userRoute.js';
+import { userRoute } from './routes/userRoute.js';
 //import { residencyRoute } from './routes/residencyRoute.js';
 dotenv.config()
 
@@ -17,3 +17,6 @@ app.use(cors())
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.use('/api/user', userRoute)
+//app.use("/api/residency", residencyRoute)
