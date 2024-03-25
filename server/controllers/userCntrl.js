b6bfb977b850744991a021e5b09813cff808e36a
@Mikist <mikeochango3@gmail.com>
2024-03-25 23:35:34 +0300

    created a bookvisit api route

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -47,3 +47,52 @@ export const bookVisit = asyncHandler(async (req, res) => {
       throw new Error(err.message);
     }
   });
+
+
+  
+// funtion to get all bookings of a user
+export const getAllBookings = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    try {
+      const bookings = await prisma.user.findUnique({
+        where: { email },
+        select: { bookedVisits: true },
+      });
+      res.status(200).send(bookings);
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  });
+
+
+  
+// function to cancel the booking
+export const cancelBooking = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    const { id } = req.params;
+    try {
+      const user = await prisma.user.findUnique({
+        where: { email: email },
+        select: { bookedVisits: true },
+      });
+  
+      const index = user.bookedVisits.findIndex((visit) => visit.id === id);
+  
+      if (index === -1) {
+        res.status(404).json({ message: "Booking not found" });
+      } else {
+        user.bookedVisits.splice(index, 1);
+        await prisma.user.update({
+          where: { email },
+          data: {
+            bookedVisits: user.bookedVisits,
+          },
+        });
+  
+        res.send("Booking cancelled successfully");
+      }
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  });
+  
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,13 +1,13 @@
 import express from "express";
-import { createUser, bookVisit} from "../controllers/userCntrl.js";
+import { createUser, bookVisit, getAllBookings, cancelBooking} from "../controllers/userCntrl.js";
 
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.post("/bookVisit/:id", bookVisit);
-//router.post("/allBookings", getAllBookings);
-//router.post("/removeBooking/:id", jwtCheck, cancelBooking);
+router.post("/allBookings", getAllBookings);
+router.post("/removeBooking/:id", cancelBooking);
 //router.post("/toFav/:rid", jwtCheck, toFav);
 //router.post("/allFav/", jwtCheck, getAllFavorites);
 export { router as userRoute };
