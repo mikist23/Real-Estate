8a2beb81a0f91b5aae49cbb1397527738ee4b326
@Mikist <mikeochango3@gmail.com>
2024-03-25 20:21:37 +0300

    created residency api route

--- server/controllers/userCntrl.js
+++ server/controllers/userCntrl.js
@@ -16,3 +16,34 @@ export const createUser = asyncHandler(async (req, res) => {
     });
   } else res.status(201).send({ message: "User already registered" });
 });
+
+
+
+// function to book a visit to residency
+export const bookVisit = asyncHandler(async (req, res) => {
+    const { email, date } = req.body;
+    const { id } = req.params;
+  
+    try {
+      const alreadyBooked = await prisma.user.findUnique({
+        where: { email },
+        select: { bookedVisits: true },
+      });
+  
+      if (alreadyBooked.bookedVisits.some((visit) => visit.id === id)) {
+        res
+          .status(400)
+          .json({ message: "This residency is already booked by you" });
+      } else {
+        await prisma.user.update({
+          where: { email: email },
+          data: {
+            bookedVisits: { push: { id, date } },
+          },
+        });
+        res.send("your visit is booked successfully");
+      }
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  });
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createUser } from "../controllers/userCntrl.js";
+import { createUser, bookVisit} from "../controllers/userCntrl.js";
 
 
 const router = express.Router();
 
 router.post("/register", createUser);
-//router.post("/bookVisit/:id", jwtCheck, bookVisit);
+router.post("/bookVisit/:id", bookVisit);
 //router.post("/allBookings", getAllBookings);
 //router.post("/removeBooking/:id", jwtCheck, cancelBooking);
 //router.post("/toFav/:rid", jwtCheck, toFav);
